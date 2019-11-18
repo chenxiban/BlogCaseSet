f6483551863d39621dd60a5a52d5ea4c9e4934cf
chenxiban <chen867647213@163.com>
2019-11-18 15:15:48 +0800

    博客案例：SpringBoot集成FreeMarker模板技术和SpringBoot全局异常处理！

--- BlogDemos/SpringBoot-FreeMarker/src/main/java/org/spring/springboot/controller/GlobalExceptionHandler.java
+++ BlogDemos/SpringBoot-FreeMarker/src/main/java/org/spring/springboot/controller/GlobalExceptionHandler.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 
- * @Description:   全局异常处理类
- * @ClassName:     GlobalExceptionHandler.java 
- * @author         Mashuai 
- * @Date           2017-10-5 下午6:27:21  
- * @Email          1119616605@qq.com
+ * @Description: 全局异常处理类
+ * @ClassName: GlobalExceptionHandler.java
+ * @author ChenYongJia
+ * @Date 2017-10-5 下午6:27:21
+ * @Email 867647213@qq.com
  */
 @ControllerAdvice
 @ResponseBody
 public class GlobalExceptionHandler {
 	
-	@ExceptionHandler(ArithmeticException.class)	//捕获所有运行时异常
-	public String exceptionHandler(){
-		return "myerr";
+	@ExceptionHandler(RuntimeException.class) // 捕获所有运行时异常
+	public String exceptionHandler() {
+		return "error";
 	}
-
+	
 }
